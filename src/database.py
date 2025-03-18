d8091d61521a035324aa1089965cfd6224e63b2b
RuslanZaripov <zaripovruslan864@gmail.com>
2025-03-18 17:20:46 +0300

    change redis creds

--- src/database.py
+++ src/database.py
@@ -22,7 +22,6 @@ class RedisClient:
             host=os.getenv('REDIS_HOST'),
             port=int(os.getenv('REDIS_PORT')),
             db=int(os.getenv('REDIS_DB')),
-            username=os.getenv('REDIS_USERNAME'),
             password=os.getenv('REDIS_PASSWORD'),
             decode_responses=True
         )
